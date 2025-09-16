ee25e5e8b90f13e1754ce84363ac50c790ecca9a
Rip_Dead <ripdead890@gmail.com>
2025-09-16 14:37:56 +0300

    .exe and .bat for release1

---
+++ endmybloatmisery.bat
@@ -0,0 +1,111 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFD54aAuBKGSoOpET/+b34OuDsXEbVfQ2e4DVlL2NL4A=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFD54aAuBKGSoOpET/+b34OuDsXE4WuZ/VZab+LiBIfRTx0z3cIQ0mH9Cnas=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title End My Bloat Misery (FAKE APP)
+
+echo Hello, Welcome to End My Bloat Misery (this doesnt delete anything)
+echo.
+echo To start, type delete-bloat
+echo.
+echo This will give us perms to delete bloat Microsoft doesn't want to let us delete
+echo.
+
+:: Fake input prompt
+set /p userinput="Type command here: "
+echo.
+echo Starting "bloat removal"...
+timeout /t 2 >nul
+echo 1 Deleting bloat... ██████████
+echo 2 Deleting bloat... ██████████
+echo 3 Deleting bloat... ██████████
+echo 4 Deleting bloat... ██████████
+echo 5 Deleting bloat... ██████████
+echo 6 Deleting bloat... ██████████
+echo 7 Deleting bloat... ██████████
+echo 8 Deleting bloat... ██████████
+echo 9 Deleting bloat... ██████████
+echo 10 Deleting bloat... ██████████
+echo 11 Deleting bloat... ██████████
+echo 12 Deleting bloat... ██████████
+echo 13 Deleting bloat... ██████████
+timeout /t 1 >nul
+echo 14 Deleting bloat... ██████████
+echo 15 Deleting bloat... ██████████
+echo 16 Deleting bloat... ██████████
+echo 17 Deleting bloat... ██████████
+echo 18 Deleting bloat... ██████████
+echo 19 Deleting bloat... ██████████
+echo 20 Deleting bloat... ██████████
+echo 21 Deleting bloat... ██████████
+echo 22 Deleting bloat... ██████████
+echo 23 Deleting bloat... ██████████
+echo 24 Deleting bloat... ██████████
+echo 25 Deleting bloat... ██████████
+echo 26 Deleting bloat... ██████████
+echo 27 Deleting bloat... ██████████
+echo 28 Deleting bloat... ██████████
+echo 29 Deleting bloat... ██████████
+echo 30 Deleting bloat... ██████████
+echo 31 Deleting bloat... ██████████
+echo 32 Deleting bloat... ██████████
+echo 33 Deleting bloat... ██████████
+echo 34 Deleting bloat... ██████████
+echo 35 Deleting bloat... ██████████
+echo 36 Deleting bloat... ██████████
+timeout /t 3 >nul
+echo 37 Deleting bloat... ██████████
+echo 38 Deleting bloat... ██████████
+echo 39 Deleting bloat... ██████████
+echo 40 Deleting bloat... ██████████
+echo 41 Deleting bloat... ██████████
+echo 42 Deleting bloat... ██████████
+timeout /t 1 >nul
+echo 43 Deleting bloat... ██████████
+echo 44 Deleting bloat... ██████████
+echo 45 Deleting bloat... ██████████
+echo 46 Deleting bloat... ██████████
+echo 47 Deleting bloat... ██████████
+echo 48 Deleting bloat... ██████████
+echo 49 Deleting bloat... ██████████
+echo 50 Deleting bloat... ██████████
+echo 51 Deleting bloat... ██████████
+echo 52 Deleting bloat... ██████████
+echo 53 Deleting bloat... ██████████
+echo 54 Deleting bloat... ██████████
+echo 55 Deleting bloat... ██████████
+echo 56 Deleting bloat... ██████████
+timeout /t 5 >nul
+echo .
+echo 56 bloat removed. Your PC is now 999%% happier!
+pause
